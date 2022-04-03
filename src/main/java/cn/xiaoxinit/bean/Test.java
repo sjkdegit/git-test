44f44076ee42d8809ac4ad84fd350e6292a7b65e
root <root1721036945@qq.com>
2022-04-03 18:58:16 +0800

    one

---
+++ src/main/java/cn/xiaoxinit/bean/Test.java
@@ -0,0 +1,11 @@
+package cn.xiaoxinit.bean;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+
+    }
+}
